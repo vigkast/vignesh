aa5dfabc6929344d11ea7e3604d702da493420aa
falwohlig <fal.wohlig@gmail.com>
2015-04-29 10:51:36 +0530

    chnage

--- AngularFramework2-master/js/controllers.js
+++ AngularFramework2-master/js/controllers.js
@@ -45,13 +45,46 @@ phonecatControllers.controller('contact', ['$scope', 'TemplateService', 'Navigat
     }
 ]);
 phonecatControllers.controller('abcd', ['$scope', 'TemplateService', 'NavigationService',
-    function($scope, TemplateService, NavigationService) {
+    function($scope, TemplateService, NavigationService, $http) {
         $scope.template = TemplateService;
         $scope.menutitle = NavigationService.makeactive("Abcd");
         TemplateService.title = $scope.menutitle;
         $scope.navigation = NavigationService.getnav();
+        $scope.footer=("views/footer.html");
     }
 ]);
+phonecatControllers.controller('myCtrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullname=function(){
+        return $scope.firstName+" "+$scope.lastName;
+    }
+});
+phonecatControllers.controller('namesCtrl', function($scope, $http) {
+    $http.get("http://www.w3schools.com/angular/customers.php")
+    .success(function(response) {$scope.names = response.records;});
+     $scope.myVar = false;
+    $scope.toggle = function() {
+        $scope.myVar = !$scope.myVar;
+    }
+});
+
+phonecatControllers.controller('mycntrl', function($scope) {
+     $scope.count = 0;
+    $scope.reset = function() {
+        $scope.count = 0;
+    }
+    $scope.reset();
+});
+
+phonecatControllers.controller('validateCtrl', function($scope) {
+    $scope.email=angular.lowercase($scope.user);
+    $scope.reset1 = function() {
+        $scope.user = "";
+        $scope.email= "";
+    }
+    $scope.reset1();
+});
 
 phonecatControllers.controller('headerctrl', ['$scope', 'TemplateService',
     function($scope, TemplateService) {
